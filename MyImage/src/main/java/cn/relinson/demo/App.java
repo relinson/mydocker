4c9ddb8da0c31f3d8ac63709e65923721b99cebf
relinson <relinson@qq.com>
2018-05-22 15:08:01 +0800

    test

--- MyImage/src/main/java/cn/relinson/demo/App.java
+++ MyImage/src/main/java/cn/relinson/demo/App.java
@@ -1,13 +1,13 @@
 package cn.relinson.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@EnableAutoConfiguration
+@SpringBootApplication
+@RestController
 public class App {
 	@RequestMapping("/")
 	@ResponseBody
